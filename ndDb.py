426d7ff7bc5f65fe357ae7c98c98a1fe718adc00
Adrian Papineau <39123870+parallelinnovation@users.noreply.github.com>
2021-06-18 11:35:20 -0400

    Update ndDb.py

--- ndDb.py
+++ ndDb.py
@@ -21,7 +21,7 @@ def new_entry():
     block_id = str(uuid.uuid4()) #generates a sring of random characters to be used as an id
     spec_id = block_id + str(uuid.uuid4())
     instance_id = spec_id + str(uuid.uuid1())
-    c.execute("INSERT INTO blockdb (dateCreated, BlockID, SpecificID, InstanceID) VALUES (?,?,?,?)", (date,block_id,spec_id,instance_id))
+    c.execute("INSERT INTO blockdb (dateCreated, BlockID, inflectionID, InstanceID) VALUES (?,?,?,?)", (date,block_id,spec_id,instance_id))
     con.commit()
 
 create_table()
