c6c7c70dbd921a69f4b117f1c6cb1779a91a4307
Adrian Papineau <39123870+parallelinnovation@users.noreply.github.com>
2021-06-18 11:34:50 -0400

    Update ndDb.py

--- ndDb.py
+++ ndDb.py
@@ -10,7 +10,7 @@ con = sqlite3.connect(path)
 c = con.cursor()
 
 def create_table(): #Creates the db file in the same directory as this file, does nothing if it already exists. 
-    c.execute('CREATE TABLE IF NOT EXISTS blockdb(BlockID TEXT, SpecificID TEXT, InstanceID TEXT, Alternate TEXT, DateCreated TEXT, DateModified TEXT, Content TEXT, PreRelation TEXT, BlockType BOOLEAN, Notable BOOLEAN, AproxMem NUMBER, BgColour TEXT, BulletStyle TEXT, Judgement TEXT, RootedStrength TEXT)') 
+    c.execute('CREATE TABLE IF NOT EXISTS blockdb(BlockID TEXT, inflectionID TEXT, InstanceID TEXT, Alternate TEXT, DateCreated TEXT, DateModified TEXT, Content TEXT, PreRelation TEXT, BlockType BOOLEAN, Notable BOOLEAN, AproxMem NUMBER, BgColour TEXT, BulletStyle TEXT, Judgement TEXT, RootedStrength TEXT)') 
     con.commit() #Gotta make sure to commit after every time you change the db. 
 
 
